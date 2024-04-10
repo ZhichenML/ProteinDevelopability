3f7b765118046b3f7f4541dc4c732ef0d4532888
ZhichenML <gzc7395219/>
2024-04-10 14:49:47 +0800

    start tap prediction

---
+++ tap.py
@@ -0,0 +1,14 @@
+from tdc.utils import retrieve_label_name_list
+label_list = retrieve_label_name_list('TAP')
+
+from tdc.single_pred import Develop
+
+data = Develop(name = 'TAP', label_name = label_list[3])
+
+split = data.get_split()
+
+train = split['train']
+valid = split['valid']
+test = split['test']
+
+import pdb; pdb.set_trace()

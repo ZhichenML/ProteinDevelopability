80a639d96899080a51c1c933b679486ff80b235b
ZhichenML <v123gz@163.com>
2024-04-12 16:28:00 +0800

    feat: add K fold cross validation

--- esm2.py
+++ esm2.py
@@ -12,6 +12,7 @@ os.environ['CUDA_VISIBLE_DEVICES']='4,6,7'
 from transformers import BertTokenizer, AutoTokenizer, BertModel, AutoModel
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 from transformers import EarlyStoppingCallback
+from transformers import set_seed
 from transformers.integrations import TensorBoardCallback
 from tdc.utils import retrieve_label_name_list
 from tdc.single_pred import Develop
@@ -21,7 +22,11 @@ from sklearn.model_selection import train_test_split
 from datasets import Dataset
 from evaluate import load
 import numpy as np
+import numpy as np
+from sklearn.model_selection import StratifiedKFold, KFold
+from datasets import load_dataset
 
+set_seed(42)
 
 metric = load("mse")
 
@@ -58,86 +63,150 @@ def load_and_process_data(path='/public/home/gongzhichen/data', name = 'tap'):
     
     return sequences, labels
 
-sequences, labels = load_and_process_data()
-import pdb; pdb.set_trace()
-train_sequences, test_sequences, train_labels, test_labels = train_test_split(sequences, labels, test_size=0.2, shuffle=True)
+def Kfold_split(train_sequences, train_labels):
+    # 存储每个 fold 的训练和验证集
+    all_fold_trainX, all_fold_trainY, all_fold_validX, all_fold_validY = [], [], [], []
+    # First make the kfold object
+    folds = KFold(n_splits=5, shuffle=True, random_state=42)
+    # import pdb; pdb.set_trace()
+    # Now make our splits based off of the labels. 
+    # We can use `np.zeros()` here since it only works off of indices, we really care about the labels
+    splits = folds.split(train_sequences)
+    # Finally, do what you want with it
+    # In this case I'm overriding the train/val/test
+    for train_idxs, val_idxs in splits:
+        fold_trainX = np.array(train_sequences)[train_idxs].tolist()
+        fold_trainY = np.array(train_labels)[train_idxs].tolist()
+        fold_validX = np.array(train_sequences)[val_idxs].tolist()
+        fold_validY = np.array(train_labels)[val_idxs].tolist()
+        
+        all_fold_trainX.append(fold_trainX)
+        all_fold_trainY.append(fold_trainY)
+        all_fold_validX.append(fold_validX)
+        all_fold_validY.append(fold_validY)
 
-model_checkpoint = '/public/home/gongzhichen/hf_models/esm150m'
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-train_tokenized = tokenizer(train_sequences)
-test_tokenized = tokenizer(test_sequences)
-
-
-train_dataset = Dataset.from_dict(train_tokenized)
-test_dataset = Dataset.from_dict(test_tokenized)
-train_dataset = train_dataset.add_column("labels", train_labels)
-test_dataset = test_dataset.add_column("labels", test_labels)
-
-
-model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, 
-                                                           num_labels=1,
-                                                           ignore_mismatched_sizes=True)
-
-# set trainer arguments
-model_name = model_checkpoint.split("/")[-1]
-batch_size = len(train_dataset)
-
-args = TrainingArguments(
-    f"{model_name}-finetuned-localization",
-    evaluation_strategy = "epoch",
-    save_strategy = "epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    num_train_epochs=400,
-    weight_decay=0.01,
-    load_best_model_at_end=True,
-    metric_for_best_model="rmse",
-    push_to_hub=False,
-    do_train= True, 
-    do_eval= True, 
-    do_predict= True, 
-    auto_find_batch_size= True,
-    logging_dir = "./log_files/", 
-    logging_strategy='epoch',
-    group_by_length = True,
-    save_total_limit=10,
-    seed=42,
-    greater_is_better=False,
-)
+    return all_fold_trainX, all_fold_trainY, all_fold_validX, all_fold_validY
 
 
+if __name__ == '__main__':
+    model_checkpoint = '/public/home/gongzhichen/hf_models/esm150m'
+    tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
 
-early_stopping = EarlyStoppingCallback(early_stopping_patience= 10, 
-                                    early_stopping_threshold= 0.001,
-                                    )
+    sequences, labels = load_and_process_data()
+    train_sequences, test_sequences, train_labels, test_labels = train_test_split(sequences, labels, test_size=0.2, shuffle=True)
+    train_sequences, test_sequences, train_labels, test_labels = train_test_split(train_sequences, train_labels, test_size=0.1, shuffle=True)
+    # all_fold_trainX, all_fold_trainY, all_fold_validX, all_fold_validY = Kfold_split(train_sequences, train_labels)
 
-trainer = Trainer(
-    model,
-    args,
-    train_dataset=train_dataset,
-    eval_dataset=test_dataset,
-    tokenizer=tokenizer,
-    compute_metrics=compute_metrics,
-    callbacks=[early_stopping,]
-)
 
-resume_from_checkpoint = None
-last_checkpoint = None
-checkpoint = None
-if resume_from_checkpoint is not None:
-    checkpoint = resume_from_checkpoint
-elif last_checkpoint is not None:
-    checkpoint = last_checkpoint
-train_result = trainer.train(resume_from_checkpoint=checkpoint)
-metrics = train_result.metrics
+    import pdb; pdb.set_trace()
 
-trainer.save_model()  # Saves the tokenizer too for easy upload
+    
+    train_tokenized = tokenizer(train_sequences)
+    test_tokenized = tokenizer(test_sequences)
 
-trainer.log_metrics("train", metrics)
-trainer.save_metrics("train", metrics)
-trainer.save_state()
-# trainer.evaluate(eval_dataset=)
 
+    train_dataset = Dataset.from_dict(train_tokenized)
+    test_dataset = Dataset.from_dict(test_tokenized)
+    train_dataset = train_dataset.add_column("labels", train_labels)
+    test_dataset = test_dataset.add_column("labels", test_labels)
+
+
+    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, 
+                                                            num_labels=1,
+                                                            ignore_mismatched_sizes=True)
+
+    # set trainer arguments
+    model_name = model_checkpoint.split("/")[-1]
+    batch_size = len(train_dataset)
+
+    args = TrainingArguments(
+        f"{model_name}-finetuned-localization",
+        evaluation_strategy = "epoch",
+        save_strategy = "epoch",
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        num_train_epochs=400,
+        weight_decay=0.01,
+        load_best_model_at_end=True,
+        metric_for_best_model="rmse",
+        push_to_hub=False,
+        do_train= True, 
+        do_eval= True, 
+        do_predict= True, 
+        auto_find_batch_size= True,
+        logging_dir = "./log_files/", 
+        logging_strategy='epoch',
+        group_by_length = True,
+        save_total_limit=10,
+        seed=42,
+        greater_is_better=False,
+    )
+
+
+
+    early_stopping = EarlyStoppingCallback(early_stopping_patience= 10, 
+                                        early_stopping_threshold= 0.001,
+                                        )
+
+    trainer = Trainer(
+        model,
+        args,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+        callbacks=[early_stopping,]
+    )
+
+    resume_from_checkpoint = None
+    last_checkpoint = None
+    checkpoint = None
+    if resume_from_checkpoint is not None:
+        checkpoint = resume_from_checkpoint
+    elif last_checkpoint is not None:
+        checkpoint = last_checkpoint
+    train_result = trainer.train(resume_from_checkpoint=checkpoint)
+    metrics = train_result.metrics
+
+    trainer.save_model()  # Saves the tokenizer too for easy upload
+
+    trainer.log_metrics("train", metrics)
+    trainer.save_metrics("train", metrics)
+    trainer.save_state()
+
+
+    do_eval = True
+    if do_eval:
+        
+        metrics = trainer.evaluate(eval_dataset=eval_dataset)
+
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    # Prediction
+    # if training_args.do_predict:
+    #     logger.info("*** Predict ***")
+    #     predictions, labels, metrics = trainer.predict(predict_dataset, metric_key_prefix="predict")
+
+    #     max_predict_samples = (
+    #         data_args.max_predict_samples if data_args.max_predict_samples is not None else len(predict_dataset)
+    #     )
+    #     metrics["predict_samples"] = min(max_predict_samples, len(predict_dataset))
+
+    #     trainer.log_metrics("predict", metrics)
+    #     trainer.save_metrics("predict", metrics)
+
+    #     predictions = np.argmax(predictions, axis=1)
+    #     output_predict_file = os.path.join(training_args.output_dir, "predictions.txt")
+    #     if trainer.is_world_process_zero():
+    #         with open(output_predict_file, "w") as writer:
+    #             writer.write("index\tprediction\n")
+    #             for index, item in enumerate(predictions):
+    #                 item = label_list[item]
+    #                 writer.write(f"{index}\t{item}\n")
 
 

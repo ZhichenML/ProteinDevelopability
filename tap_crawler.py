1b05f905d9dc1e0eeb31ee873ad7c7c9b0789353
ZhichenML <v123gz@163.com>
2024-05-14 13:00:21 +0800

    crawler newer

--- tap_crawler.py
+++ tap_crawler.py
@@ -1,102 +1,100 @@
 import requests
-import json
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from fake_useragent import UserAgent
-import requests
 from bs4 import BeautifulSoup
-import time
 import pandas as pd
+import time
+import logging
 
+def setup_logging():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        handlers=[logging.FileHandler('sab_tap.log'), logging.StreamHandler()])
 
-def submit_hlchain_to_tap(heavy_chain_sequence = "QVKLQESGAELARPGASVKLSCKASGYTFTNYWMQWVKQRPGQGLDWIGAIYPGDGNTRYTHKFKGKATLTADKSSSTAYMQLSSLASEDSGVYYCARGEGNYAWFAYWGQGTTVTVSS", 
-                    light_chain_sequence = "DIELTQSPASLSASVGETVTITCQASENIYSYLAWHQQKQGKSPQLLVYNAKTLAGGVSSRFSGSGSGTHFSLKIKSLQPEDFGIYYCQHHYGILPTFGGGTKLEIK"):
-
-    # 目标URL
+def submit_hlchain_to_tap(heavy_chain_sequence, light_chain_sequence):
     url = "https://opig.stats.ox.ac.uk/webapps/sabdab-sabpred/sabpred/tap"
-
-    header = {'authority':'opig.stats.ox.ac.uk',
-    'method': 'POST',
-    'path': '/webapps/sabdab-sabpred/sabpred/tap',
-    'scheme': 'https',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Content-Type':'application/x-www-form-urlencoded',
-    'Referer': 'https://opig.stats.ox.ac.uk/webapps/sabdab-sabpred/sabpred/tap',
-    'Origin':'https://opig.stats.ox.ac.uk',
-    'Cookie':'_ga_R913GG5VNB=GS1.1.1715565431.1.0.1715565431.0.0.0; _ga=GA1.3.934786908.1715565431; _gid=GA1.3.448289092.1715565432; _gat_gtag_UA_142143190_1=1',
-    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Referer': 'https://opig.stats.ox.ac.uk/webapps/sabdab-sabpred/sabpred/tap',
+        'Origin': 'https://opig.stats.ox.ac.uk',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
     }
 
-    # 构建要提交的数据
     data = {"hchain": heavy_chain_sequence, "lchain": light_chain_sequence}
-  
-    
-    response = requests.post(url, headers=header, data=data) 
-    response.raise_for_status()
 
-    # 检查请求是否成功
-    if response.status_code == 200:
-        print("序列数据提交成功！")
-    else:
-        print("序列数据提交失败，状态码：", response.status_code)
+    with requests.Session() as session:
+        try:
+            response = session.post(url, headers=headers, data=data)
+            response.raise_for_status()
+            logging.info("Sequence data submitted successfully!")
 
-    time.sleep(50)
-    # 目标网址
-    res_url = response.url
-    
-    # 发送HTTP请求
-    response = requests.get(res_url)
+            time.sleep(50)  # Wait for the server to process the request
 
-    # 确保请求成功
-    if response.status_code == 200:
-        # 使用BeautifulSoup解析HTML内容
-        soup = BeautifulSoup(response.content, 'html.parser')
-        
-        # 找到class为log_file_box的div标签
-        log_file_box = soup.find('div', class_='log_file_box')
-        
-        # 检查是否找到了该标签
-        if log_file_box:
-            # 获取该标签内的文本内容
-            log_text = log_file_box.get_text(strip=True)
-            ind = log_text.find('Summary') 
-            res = log_text[ind:]
-            
-        else:
-            print('未能找到指定的div标签。')
-    else:
-        print('请求网页失败，状态码:', response.status_code)
-    
+            response = session.get(response.url)
+            response.raise_for_status()
+
+            soup = BeautifulSoup(response.content, 'html.parser')
+            log_file_box = soup.find('div', class_='log_file_box')
+            if log_file_box:
+                log_text = log_file_box.get_text(strip=True)
+                ind = log_text.find('Summary')
+                res = log_text[ind:] if ind != -1 else "Summary not found"
+            else:
+                logging.warning('Could not find the specified div tag.')
+                res = "Log file box not found"
+
+        except requests.RequestException as e:
+            logging.error(f"Request failed: {e}")
+            res = "Request failed"
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            res = "An error occurred"
+
     res = ' '.join(res.split())
-    res = res[res.find('Total IMGT CDR Length'):]
+    res = res[res.find('Total IMGT CDR Length'):] if 'Total IMGT CDR Length' in res else res
     res = res.replace('(GREEN flag)', '\n')
-    import pdb; pdb.set_trace()
-    res =  [ [v.split(':')[0].strip(), v.split(':')[1].strip()]  for v in res.split('\n')[:-1] ]
-    res = pd.DataFrame(res, columns=['TAP', 'value'])
-    
-    return res
+    res_list = [v.split(':') for v in res.split('\n')[:-1]]
+    res_stripped = [[element.strip(' score is') for element in sublist] for sublist in res_list]
 
+    # Extract column names from the first row of res_stripped
+    columns = [col[0] for col in res_stripped]
+
+    # Extract data rows
+    data_rows = [col[1] for col in res_stripped]
+
+    # Create DataFrame
+    res_df = pd.DataFrame([data_rows], columns=columns)
+
+    # Add heavy and light sequences as new columns
+    res_df['Heavy Sequence'] = heavy_chain_sequence
+    res_df['Light Sequence'] = light_chain_sequence
+
+    # Reorder columns to move 'Heavy Sequence' and 'Light Sequence' to the left
+    columns_order = ['Heavy Sequence', 'Light Sequence'] + [col for col in res_df.columns if col not in ['Heavy Sequence', 'Light Sequence']]
+    res_df = res_df[columns_order]
+    
+    return res_df
 
 def main():
-    path = r'C:\Users\A\Desktop\sab_tap\TheraSAbDab_SeqStruc_OnlineDownload.csv'
-    df = pd.read_csv(path)
-    
+    setup_logging()
+    input_path = r'C:\Users\A\Desktop\sab_tap\TheraSAbDab_SeqStruc_OnlineDownload.csv'
+    output_path = r'C:\Users\A\Desktop\sab_tap\TheraSAbDab_Results.csv'
+
+    df = pd.read_csv(input_path)
+    results = []
+
     for i in range(len(df)):
         heavy_chain_sequence = df.loc[i, 'Heavy Sequence']
         light_chain_sequence = df.loc[i, 'Light Sequence']
-        res = submit_hlchain_to_tap(heavy_chain_sequence, light_chain_sequence)
-        import pdb; pdb.set_trace()
-        print(res)
-
+        logging.info(f"Processing sequence {i + 1}/{len(df)}")
+        res_df = submit_hlchain_to_tap(heavy_chain_sequence, light_chain_sequence)
+        
+        
+        results.append(res_df)
+        time.sleep(5)  # Wait before sending the next request
+    
+    result_df = pd.concat(results, ignore_index=True)
+    result_df.to_csv(output_path, index=False)
+    logging.info("Results saved to CSV")
 
 if __name__ == '__main__':
     main()
-    
-
-
-
